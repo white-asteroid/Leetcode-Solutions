f49869484111d4eb1642da872f7f3ea1226ade44
shivam garg <58942078+white-asteroid@users.noreply.github.com>
2022-07-13 00:36:02 +0530

    Time: 3 ms (92.05%), Space: 128.4 MB (28.22%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+         // ArrayList<Integer> arrli = new ArrayList<Integer>(arr.length);
+        
+        for(int i =0;i<arr.length;i++)
+        {
+            int cor = arr[i]-1;
+            if(arr[i]>0 && arr[i]<= arr.length && arr[i]!=arr[cor])
+            {
+                swap(arr,i,cor);
+                i--;
+            }
+        }
+        // System.out.println(Arrays.toString(arr));
+        for(int i =0;i<arr.length;i++)
+        {
+            
+            if(arr[i]!=i+1)
+                return i+1;
+        }
+        
+        
+        return arr.length+1;
+        
+        
+    }
+     public void swap(int[] arr,int i , int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
