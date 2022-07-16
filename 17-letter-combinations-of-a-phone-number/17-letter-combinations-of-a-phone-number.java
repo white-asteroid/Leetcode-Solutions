a2b34d365fa5dde6390dc3fb8da289dc6222c4ce
shivam garg <58942078+white-asteroid@users.noreply.github.com>
2022-07-16 19:53:39 +0530

    Time: 8 ms (33.07%), Space: 43.7 MB (6.32%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -2,24 +2,25 @@ class Solution {
    
      public List<String> letterCombinationsRec(String p,String up){
        
-		if(up == "") {
+		if(up == "") { 
 			ArrayList<String> list = new ArrayList<String>();
 			list.add(p);
 			return list;
 		}
 		ArrayList<String> list = new ArrayList<String>();
-		int d = up.charAt(0) - '0'; 
-		int i = (d-2)*3;
+		int d = up.charAt(0) - '0'; //cpnvert string to numeric value 
+		int i = (d-2)*3;    // Calculating the character we need to start with
+                            //for ex if we have "4" we need to start from ('a'+ i)ASCII
 		int len = i+3;
          if(d>7) {
 			i=(d-2)*3 + 1;
-			len++;
+			len++;          // cause we are changing va;ue of 1 you can change this to l =i+3
 		}
 		if(d==7 || d==9) {
-			len = i+4;
+			len = i+4;    // for 7 and 9 we have for letters
 		}
 		
-		for(;i<len;i++)
+		for(;i<len;i++)  // run the loop according to number of letters in that key 
 		{
 //			ArrayList<String> list = new ArrayList<String>();
 			char ch = (char)('a'+i);
