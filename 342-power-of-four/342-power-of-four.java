87f448f47694d6755274c9e0f3c751936ced73b4
shivam garg <58942078+white-asteroid@users.noreply.github.com>
2022-07-28 20:56:31 +0530

    Time: 3 ms (11.41%), Space: 41.2 MB (55.85%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -2,10 +2,7 @@ class Solution {
     public boolean isPowerOfFour(int n) {
         if(n==0)
             return false;
-        if(n==1 || n==4)
-            return true;
         double x = Math.log(n) / Math.log(4);
-        // System.out.println(x);
         if(x%1==0)
             return true;
         return false;
