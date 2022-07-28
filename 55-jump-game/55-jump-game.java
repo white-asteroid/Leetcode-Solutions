dfce9f08fa0862fe889276fd797134e0c60ed85e
shivam garg <58942078+white-asteroid@users.noreply.github.com>
2022-07-28 22:42:24 +0530

    Time: 4 ms (47.23%), Space: 67.6 MB (61.27%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -4,19 +4,15 @@ class Solution {
             return true;
         if(arr[0]==0)
             return false;
-        int limit= arr[0], cj = arr[0];
-        
+        int limit= arr[0], cj = arr[0]; // 'c'an 'j'ump is the number we can jump
+                                        // limit is the index upto which we can jump as of now
         for(int i=0;i<arr.length-1 && i<=limit ;i++){
-            cj = Math.max(cj,arr[i]);
-           
-            limit = i+cj;
-             // cj--;
-            // System.out.println("cj : "+cj+" limit : "+limit+" i : "+i);
-            if(i+arr[i] >= arr.length-1 )
+            cj = Math.max(cj,arr[i]);   //checking if ability to jump(cj) is increased
+            limit = i+cj;               // "i" is the current block and cj is power to jump
+                                        
+            if(i+arr[i] >= arr.length-1 ) 
                 return true;
-            
-            // if(arr)
-            cj-=1;
+            cj-=1;                      //since we jumped 1 box our ability to jump reduces by 1
         }
         return false;
     }
