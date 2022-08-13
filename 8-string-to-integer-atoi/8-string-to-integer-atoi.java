cafb729cdccdf22ee37debf411188419341af779
shivam garg <58942078+white-asteroid@users.noreply.github.com>
2022-08-13 16:18:00 +0530

    Time: 3 ms (69.40%), Space: 43.1 MB (48.71%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -1,39 +1,34 @@
 class Solution {
     public int myAtoi(String s) {
-        if(s.isEmpty())
+        if(s.isEmpty())  // see if string is empty
             return 0;
          int i = 0,m=1,res=0,prev=0;
-        while(i<s.length() && s.charAt(i) == ' '){
-            i++;
+        while(i<s.length() && s.charAt(i) == ' '){  
+            i++; //ignore all the white spaces
         }
-        if(i<s.length() && s.charAt(i)== '-') {
+        if(i<s.length() && s.charAt(i)== '-') {  //check if there is any sign 
             m = -1;
             i++;
         }
         else if (i<s.length() && s.charAt(i)== '+') {
-//            m= 1;
             i++;
-        }
+        }                                          //sign check end
 
         //first digit
-        if(i<s.length() && (s.charAt(i)<'0' || s.charAt(i) > '9'))
+        if(i<s.length() && (s.charAt(i)<'0' || s.charAt(i) > '9')) // first digit cant be other than numbers i.e if first character is a letter return 0
             return 0;
-        while(i<s.length() && s.charAt(i)=='0'){
+        while(i<s.length() && s.charAt(i)=='0'){ // check if first digit is zero
             i++;
         }
         int t ,c =0;
         boolean f =false;
-        if(i<s.length() && i<s.length())
-            prev = s.charAt(i) - '0';
+        if(i<s.length())   // MAIN STUFF
+            
         while(i<s.length() && s.charAt(i)>='0' && s.charAt(i) <= '9'){
-
-            t =s.charAt(i) -'0';
-          
-
-            // System.out.println(res+ "t : "+t);
-            // System.out.print("Condition : ");
-            // System.out.println((res >  Integer.MAX_VALUE / 10 || (res ==  Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > 7)));
-            if(res >  Integer.MAX_VALUE/10  || (res ==  Integer.MAX_VALUE/10 && s.charAt(i) - '0' > 7))
+        // this while condition will check if string contains only numbers
+            t =s.charAt(i) -'0'; // this converts char to int
+            //INT_MAX = 2147483648
+            if(res >  Integer.MAX_VALUE/10  || (res ==  Integer.MAX_VALUE/10 && s.charAt(i) - '0' > 7)) //if result is greater than 214748364 than string can have number greater than MAX_INT so check if last digit is also grt than 7 (Read description if your confused)
             {
                 res =0;
                 f = true;
@@ -41,14 +36,13 @@ class Solution {
             }
             i++;
             c++;
-            prev = res;
             res*=10;
-              res += t;
-            if(i== s.length())
+            res += t;
+            if(i== s.length()) // just extra checking
                 break;
             
         }
-        if(f)
+        if(f)             // if string s is not in range
         {
             if(m==1)
                 return 2147483647;
